cf660ab7ebe26751cb4348ab4f2713a788a4c860
zednaked <zednaked@gmail.com>
2022-10-13 20:42:17 -0300

    massiva

--- sketch.js
+++ sketch.js
@@ -1,9 +1,122 @@
+//primeiramente iremos fazer uma engine que abra  os meus sprites quebrados
+let splayer;
+let fundo;
 
+let gsprites
+
+let imgplayer;
+let imgcabeca;
+let buffer;
+let clicks;
+
+let zoomz = 4
+
+
+function preload(){
+
+
+  imgplayer = loadImage (
+    "/Arte/Sprites/Player/meninagato.png");
+
+   
+}
+
+function keyReleased(){ 
+
+if (keyCode === LEFT_ARROW)
+{
+  splayer.scale +=1
+  zoomz += 1
+}else if (keyCode === RIGHT_ARROW)
+{
+  splayer.scale -= 1
+  zoomz -= 1
+ß
+}
+
+
+
+}
+
+function mouseClicked()
+{
+ 
+   //imgplayer = crop (imgplayer,54,33,24,21)
+
+
+  clicks.push ({
+      x : mouseX,
+      y : mouseY
+    });
+  if (clicks.length == 2)
+  {
+   let xini = round(clicks[0].x/zoomz) 
+   let yini = round(clicks[0].y/zoomz)
+   let wini = round((clicks[1].x - clicks[0].x)/zoomz)
+   let hini = round((clicks[1].y - clicks[0].y)/zoomz)
+   let ani = splayer.animation
+   let imagem = ani.images[0]
+   let vetorimg = crop (imagem,xini,yini,wini,hini)
+
+    //cria novo sprite e separa
+    let pedaco = new Sprite ()
+    
+    pedaco.addImage(vetorimg); 
+    gsprites.add (pedaco)
+ 
+    pedaco.scale = zoomz
+    pedaco.h = hini
+    pedaco.w = wini
+    pedaco.x = width/2
+    pedaco.y = 20
+
+    clicks = []
+   
+  }
+}
+
+
+function crop(image, x, y, w, h) {
+  let cropped = createImage(w, h);
+  noSmooth()
+  cropped.copy(image, x, y, x + w, y + h, 0, 0, x + w, y + h);
+ 
+
+  return cropped;
+}
 
 function setup() {
-  createCanvas(400, 400);
+  clicks = [];
+  gsprites = new Group();
+  createCanvas(500, 500);
+
+  //rectMode()
+  strokeWeight(1); 
+  //imgplayer = crop(imgplayer,0,0,60,60);
+  camera.on();
+  //camera.zoom = zoomz;
+
+
+  splayer = new Sprite ();
+  splayer.overlap(allSprites);
+  push()
+  splayer.addAni (imgplayer);
+  
+  
+  pop()
+  splayer.scale =zoomz
+  splayer.x = width/2;
+  splayer.y = height/2;
+
+  
 }
 
 function draw() {
-  background(220);
+//clear()
+  noSmooth();
+  background(150)
+  camera.on();
+  //camera.zoom = zoomz;
+  //tint(0, 153, 204);
+  allSprites.draw();
 }
---
+++ sketch2.js
@@ -0,0 +1,166 @@
+let gchar
+let personagem
+function preload ()
+{
+   
+    gchar = new Group()
+    personagem = {
+        nome: "Menina Gato",
+        velocidade: 20,
+        x:width/2,//offset x da posicao
+        y:height/2,//offset y da posicao
+        corpo:{
+            katana: new Sprite(loadImage("Arte/Sprites/Player/meninagato/katana.png")),
+            rabo: new Sprite(loadImage("Arte/Sprites/Player/meninagato/rabo.png")),
+            bracoesquerdo: new Sprite(loadImage("Arte/Sprites/Player/meninagato/bracoesquerdo.png")),
+            bracodireito: new Sprite(loadImage("Arte/Sprites/Player/meninagato/bracodireito.png")),
+            pernas: new Sprite(loadImage("Arte/Sprites/Player/meninagato/pernas.png")),
+            tronco: new Sprite(loadImage("Arte/Sprites/Player/meninagato/tronco.png")),
+            cabeca: new Sprite(loadImage("Arte/Sprites/Player/meninagato/cabeca.png")),
+            sombra: new Sprite(loadImage("Arte/Sprites/Player/meninagato/sombra.png"))
+        }
+    };
+}
+
+//funcao que anima o corpo do personagem
+function doAnim(){
+
+
+
+}
+
+function mouseClicked()
+{
+   
+}
+
+function moveDireita (qtd)
+{
+    personagem.corpo.cabeca.x += qtd
+    personagem.corpo.tronco.x += qtd
+    personagem.corpo.bracodireito.x += qtd
+    personagem.corpo.bracoesquerdo.x += qtd
+    personagem.corpo.katana.x += qtd 
+    personagem.corpo.pernas.x += qtd
+    personagem.corpo.rabo.x += qtd 
+    personagem.corpo.sombra.x += qtd
+}
+
+function moveEsquerda (qtd)
+{
+    personagem.corpo.cabeca.x -= qtd
+    personagem.corpo.tronco.x -= qtd
+    personagem.corpo.bracodireito.x -= qtd
+    personagem.corpo.bracoesquerdo.x -= qtd
+    personagem.corpo.katana.x -= qtd 
+    personagem.corpo.pernas.x -= qtd
+    personagem.corpo.rabo.x -= qtd 
+    personagem.corpo.sombra.x -= qtd
+}
+
+function moveCima (qtd)
+{
+    personagem.corpo.cabeca.y -= qtd
+    personagem.corpo.tronco.y -= qtd
+    personagem.corpo.bracodireito.y -= qtd
+    personagem.corpo.bracoesquerdo.y -= qtd
+    personagem.corpo.katana.y -= qtd 
+    personagem.corpo.pernas.y -= qtd
+    personagem.corpo.rabo.y -= qtd 
+    personagem.corpo.sombra.y -= qtd
+}
+function moveBaixo (qtd)
+{
+    personagem.corpo.cabeca.y += qtd
+    personagem.corpo.tronco.y += qtd
+    personagem.corpo.bracodireito.y += qtd
+    personagem.corpo.bracoesquerdo.y += qtd
+    personagem.corpo.katana.y += qtd 
+    personagem.corpo.pernas.y += qtd
+    personagem.corpo.rabo.y += qtd 
+    personagem.corpo.sombra.y += qtd
+}
+
+function keyReleased()
+{
+    switch (keyCode){
+        case RIGHT_ARROW:
+            moveDireita (personagem.velocidade)
+            break;
+        case LEFT_ARROW:
+            moveEsquerda(personagem.velocidade)
+            break;
+        case UP_ARROW:
+            moveCima(personagem.velocidade)
+            break
+        case DOWN_ARROW:
+            moveBaixo(personagem.velocidade)
+            break
+    }
+
+
+}
+
+function setup()
+{
+   
+    createCanvas (500,500);
+    noSmooth()
+    personagem.corpo.cabeca.x = width/2;
+    personagem.corpo.cabeca.y = height/2;
+    personagem.corpo.cabeca.overlap(allSprites)
+
+    personagem.corpo.tronco.x = width/2;
+    personagem.corpo.tronco.y = height/2+8;
+    personagem.corpo.tronco.overlap(allSprites)
+
+    personagem.corpo.bracoesquerdo.x = width/2- 9;
+    personagem.corpo.bracoesquerdo.y = height/2+8;
+    personagem.corpo.bracoesquerdo.overlap(allSprites)
+
+    personagem.corpo.bracodireito.x = width/2+ 8;
+    personagem.corpo.bracodireito.y = height/2+9;
+    personagem.corpo.bracodireito.overlap(allSprites)
+
+    personagem.corpo.pernas.x = width/2-1;
+    personagem.corpo.pernas.y = height/2+17;
+    personagem.corpo.pernas.overlap(allSprites)
+
+    personagem.corpo.rabo.x = width/2+13.5;
+    personagem.corpo.rabo.y = height/2+4;
+    personagem.corpo.rabo.overlap(allSprites)
+
+    personagem.corpo.katana.x = width/2+10;
+    personagem.corpo.katana.y = height/2-5;
+    personagem.corpo.katana.overlap(allSprites)
+
+    let ani = personagem.corpo.cabeca.animation
+    let imagem = ani.images[0]
+  
+    p5.tween.manager
+        .addTween (personagem.corpo.tronco)
+        .addMotion ('scale', 1.03,1000,'easeInOutQuint')
+        .startLoop()
+    p5.tween.manager
+        .addTween (personagem.corpo.cabeca)
+        .addMotion ('scale', 1.01,2000,'easeInOutQuint')
+        .startLoop()
+    p5.tween.manager
+        .addTween (personagem.corpo.bracodireito)
+        .addMotion ('scale', 1.01,2000,'easeInOutQuint')
+        .startLoop()
+        
+}
+
+function draw()
+{
+background (155)
+camera.on()
+camera.zoom = 5
+allSprites.draw()
+
+//o codigo abaixo possibilita vc mudar de cor apenas um pedaço do personagem
+//tint (200,10,10)
+//personagem.corpo.cabeca.draw()
+
+}
